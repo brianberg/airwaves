d9526efb3bfd2e433246c450c79181260886a19e
Brian Berg <brianberg.se@gmail.com>
2018-10-26 14:12:53 -0500

    feat(radio): add tuning
    - uses rtl_fm and aplay, notifies if not found
    - add sample rate and output rate

--- src/radio.ts
+++ src/radio.ts
@@ -1,8 +1,18 @@
+import * as exists from "command-exists";
+import * as execa from "execa";
+
 export default class Radio {
   private static instance: Radio;
 
   private frequency: number;
-  
+  private sampleRate: number = 200000;
+  private outputRate: number = 480000;
+
+  private childProcess: execa.ExecaChildProcess;
+
+  private tunerExists: boolean = false;
+  private audioExists: boolean = false;
+
   public static getInstance() {
     if (!Radio.instance) {
       Radio.instance = new Radio();
@@ -12,10 +22,40 @@ export default class Radio {
 
   private constructor() {
     this.frequency = 105.5;
+    this.tunerExists = exists.sync("rtl_fm");
+    this.audioExists = exists.sync("aplay");
+    if (!this.tunerExists) {
+      console.info("rtl_fm not found");
+    }
+    if (!this.audioExists) {
+      console.info("aplay not found");
+    }
   }
 
   public getFrequency() {
     return this.frequency;
+  }
+
+  public getSampleRate() {
+    return this.sampleRate;
+  }
+
+  public getOutputRate() {
+    return this.outputRate;
+  }
+
+  public setSampleRate(sampleRate: number) {
+    this.sampleRate = sampleRate;
+  }
+
+  public setOutputRate(outputRate: number) {
+    this.outputRate = outputRate;
+  }
+
+  public stop() {
+    if (this.childProcess) {
+      this.childProcess.kill();
+    }
   }
 
   public tune(frequency: number) {
@@ -36,6 +76,15 @@ export default class Radio {
   }
 
   private _tune() {
-    // TODO: tune and play audio
+    if (this.tunerExists && this.audioExists) {
+      this.stop();
+      const radioCmd =
+        "rtl_fm " +
+        `-f ${this.frequency}M ` +
+        `-s ${this.sampleRate} ` +
+        `-r ${this.outputRate}`;
+      const audioCmd = `aplay -r ${this.outputRate} -f S16_LE`;
+      this.childProcess = execa(`${radioCmd} | ${audioCmd}`);
+    }
   }
 }
